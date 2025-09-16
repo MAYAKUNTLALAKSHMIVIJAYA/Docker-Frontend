13303fc9a1fd36122ac7d5eb945ebb0450e4256e
MAYAKUNTLALAKSHMIVIJAYA <mayakuntlalakshmi216@gmail.com>
2025-09-16 12:31:05 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://52.201.182.126/api/products";  // Backend API URL
+const API_BASE_URL = "http://52.201.182.126:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
